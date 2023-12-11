dd95f19f05beaa3687f168a06eaf23c780fb4875
Sergio Esteban <sergio.calvo@udea.edu.co>
2023-12-11 12:33:03 -0500

    prueba react-Server

--- src/components/products/product.controller.js
+++ src/components/products/product.controller.js
@@ -123,10 +123,30 @@ const deleteProduct = (req,res) =>{
     }
 }
 
+const bancostodos = (req,res) =>{
+    try {
+        let sql = 'SELECT * FROM bank'
+        db.query(sql,(err,rows,field) =>{
+            if(!err){
+                if(rows.length < 1){
+                    res.json({data: `Error no found products`})
+                } else {
+                    res.json({data: rows})
+                }
+            } else {
+                throw err
+            }
+        })
+    } catch (err) {
+        console.log({data: `Internal Server Error: ${err}`})
+    }
+}
+
 module.exports = {
     getAllProduct,
     getProduct,
     createProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    bancostodos
 }
